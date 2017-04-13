92604af599b7dcda609c7467f95f512deb30b074
downmooner <11she_232@163.com>
2017-04-13 21:09:24 +0800

    fix const operator()  tarit in make cleaner for lambda

--- cleaner.h
+++ cleaner.h
@@ -9,14 +9,34 @@
 #include <memory>
 #include <type_traits>
 
+
+#ifdef XHBLIB_CLEANER_UNIT_TEST
+
+#include <string>
+#include <sstream>
+#include <iostream>
+
+using std::string;
+using std::stringstream;
+using std::cout;
+using std::endl;
+
+string assert_tags;
+stringstream assert_ss;
+
+#define XHBLIB_UNIT_TEST(tag) { assert_ss << tag; cout<<tag<<endl;}
+
+#else
+
+#define XHBLIB_UNIT_TEST(tag) 0
+
+#endif
+
+
+
 class cleaner final{
 public:
-    struct impl{
-        unsigned int refs = 1;
-        cleaner next;
-        impl(cleaner n):next(std::move(n)){}
-        virtual ~impl(){}  
-    };
+    struct impl;
     struct cleaner_raw_point_tag{};
 private:
     // 优化，很多时候只是管理缓存，且没有share情况,new char 等，这时直接使用hack过的地址，而不是impl结构体。更为节省内存
@@ -26,22 +46,12 @@ public:
     cleaner() = default;
     cleaner(const cleaner&) = delete;
 
-    cleaner(cleaner&& other)noexcept : _impl(other._impl){other._impl == nullptr;}
+    cleaner(cleaner&& other)noexcept : _impl(other._impl){other._impl = nullptr;}
     
     explicit cleaner(impl* i):_impl(i){}
     explicit cleaner(void* ptr,cleaner_raw_point_tag):_impl(from_raw_point(ptr)){}
 
-    ~cleaner(){
-        if(_impl == nullptr) return;
-
-        if(is_raw_point()){
-            std::free(to_raw_point());
-            return;
-        }
-
-        if( --_impl->refs == 0)
-            delete _impl;
-    }
+    ~cleaner();
 
     cleaner& operator=(const cleaner&) = delete;
     cleaner& operator=(cleaner&& other){
@@ -52,13 +62,7 @@ public:
         return *this;
     }
     // share 返回一个指向相同的cleaner,内部引用计数+1,封装的动作将在2个cleaner都析构后执行。
-    inline cleaner share(){
-        if(_impl == nullptr) return cleaner();
-        if(is_raw_point())
-            _impl = new free_cleaner_impl(to_raw_point());
-        ++_impl.refs;
-        return cleaner(_impl);
-    }
+    cleaner share();
 
 private:
     static bool is_raw_point(impl* i){
@@ -77,16 +81,41 @@ private:
     }
     impl* from_raw_point(void* p){
         auto x = reinterpret_cast<uintptr_t>(p);
-        return reinterpret_cast<void*>(x | 1);
+        return reinterpret_cast<impl*>(x | 1);
     }
 
 };
+
+struct cleaner::impl{
+    unsigned int refs = 1;
+    cleaner next;
+    impl(cleaner n):next(std::move(n)){}
+    virtual ~impl(){}
+};
+
+cleaner::~cleaner(){
+    if(_impl == nullptr) return;
+    
+    if(is_raw_point()){
+        std::free(to_raw_point());
+        XHBLIB_UNIT_TEST('f');
+        return;
+    }
+    
+    if( --_impl->refs == 0){
+        delete _impl;
+    }
+}
+
 
 template<typename Action>
 struct lambda_cleaner_impl final : public cleaner::impl{
     Action act;
     lambda_cleaner_impl(cleaner next,Action&& a):impl(std::move(next)),act(std::move(a)){}
-    virtual ~lambda_cleaner_impl() override {act();}
+    virtual ~lambda_cleaner_impl() override {
+        act();
+        XHBLIB_UNIT_TEST('l');
+    }
 };
 
 template<typename Action>
@@ -97,18 +126,49 @@ lambda_cleaner_impl<Action>* make_lambda_cleaner_impl(cleaner next,Action&& act)
 
 template<typename Action>
 cleaner make_lambda_cleaner(Action && act){
-    return make_lambda_cleaner_impl(cleaner(),std::move(act));
+    return cleaner(make_lambda_cleaner_impl(cleaner(),std::move(act)));
 }
 
 template<typename Action>
 cleaner make_lambda_cleaner(cleaner next,Action && act){
-    return make_lambda_cleaner_impl(std::move(next),std::move(act));
+    return cleaner(make_lambda_cleaner_impl(std::move(next),std::move(act)));
 }
+
+
+struct free_cleaner_impl final : public cleaner::impl{
+    void* ptr;
+    free_cleaner_impl(void* p):impl(cleaner()),ptr(p){}
+    virtual ~free_cleaner_impl()override{
+        if(ptr != nullptr)std::free(ptr);
+        XHBLIB_UNIT_TEST('f');
+    }
+};
+
+inline cleaner make_free_cleaner(void* ptr){
+    if(ptr == nullptr) return cleaner();
+    return cleaner(ptr,cleaner::cleaner_raw_point_tag());
+}
+
+inline cleaner make_free_cleaner(cleaner next,void* ptr){
+    return make_lambda_cleaner(std::move(next),[ptr]()mutable{std::free(ptr);});
+}
+
+inline cleaner cleaner::share(){
+    if(_impl == nullptr) return cleaner();
+    if(is_raw_point())
+        _impl = new free_cleaner_impl(to_raw_point());
+    ++_impl->refs;
+    return cleaner(_impl);
+}
+
 
 template<typename Object>
 struct object_cleaner_impl final : public cleaner::impl{
     Object obj;
-    object_cleaner_impl(cleaner next,object&& o):impl(std::move(next)),obj(std::move(o)){}
+    object_cleaner_impl(cleaner next,Object&& o):impl(std::move(next)),obj(std::move(o)){}
+    virtual ~object_cleaner_impl() override {
+        XHBLIB_UNIT_TEST('o');
+    }
 };
 
 template<typename Object>
@@ -127,39 +187,77 @@ cleaner make_object_cleaner(cleaner d,T&& obj){
     return cleaner{make_object_cleaner_impl(std::move(d),std::move(obj))};
 }
 
-struct free_cleaner_impl final : public cleaner::impl{
-    void* ptr;
-    free_cleaner_impl(void* p):impl(cleaner()),ptr(p){}
-    virtual ~free_cleaner_impl()override{if(ptr != nullptr)std::free(ptr);}
+
+template<typename, typename T>
+struct has_serialize {
+    static_assert(
+                  std::integral_constant<T, false>::value,
+                  "Second template parameter needs to be of function type.");
 };
 
-inline cleaner make_free_cleaner(void* ptr){
-    if(ptr == nullptr) return cleaner();
-    return cleaner(ptr,cleaner::cleaner_raw_point_tag());
-}
+// helper
 
-inline cleaner make_free_cleaner(cleaner next,void* ptr){
-    return make_lambda_cleaner(std::move(next),[ptr]()mutable{std::free(ptr);});
-}
+template <class C>
+class has_function_operator{
+    template <class T>
+    static std::true_type test_sign(void (T::*)(void));
+    
+    template <class T>
+    static std::true_type test_sign(void (T::*)(void)const);
+    
+    template <class T>
+    static decltype(test_sign(&T::operator())) test(std::nullptr_t);
+    
+    template <class T>
+    static std::false_type test(...);
+    
+public:
+    using type = decltype(test<C>(nullptr));
+    static const bool value = type::value;
+};
+
 
 
 // free
 template<typename T>
-cleaner make_cleaner(cleaner next,T* obj,std::enable_if<std::is_pod<T>::value>::type* = nullptr){
+cleaner make_cleaner(cleaner next,T* obj,typename std::enable_if<std::is_pod<T>::value ||
+                                            std::is_void<T>::value>::type* = nullptr){
     return make_free_cleaner(std::move(next),static_cast<void*>(obj));
 }
-
-// lambda
 template<typename T>
-cleaner make_cleaner(cleaner next,T&& obj,std::enable_if<std::is_trivially_destructible<T>::value && 
-                                                        std::is_invocable<T>::value>::type* = nullptr){
+cleaner make_cleaner(T* obj,typename std::enable_if<std::is_pod<T>::value ||
+                                std::is_void<T>::value>::type* = nullptr){
+    return make_free_cleaner(static_cast<void*>(obj));
+}
+
+
+
+// lambda void() 
+template<typename T>
+cleaner make_cleaner(cleaner next,T&& obj,typename std::enable_if<has_function_operator<typename std::remove_cv<typename std::remove_reference<T>::type>::type>::value ||
+                                                        (std::is_function<typename std::remove_reference<T>::type>::value &&
+                                                            std::is_same<typename std::remove_reference<T>::type,void()>::value)>::type* = nullptr){
     return make_lambda_cleaner(std::move(next),std::move(obj));
+}
+template<typename T>
+cleaner make_cleaner(T&& obj,typename std::enable_if<(std::is_function<typename std::remove_reference<T>::type>::value &&
+                                                      std::is_same<typename std::remove_reference<T>::type,void()>::value) ||
+                            has_function_operator<typename std::remove_reference<T>::type>::value>::type* = nullptr){
+    return make_lambda_cleaner(std::move(obj));
 }
 
 // obj
 template<typename T>
-cleaner make_cleaner(cleaner next,T&& obj,std::enable_if<!std::is_trivially_destructible<T>::value && 
-                                                        !std::is_invocable<T>::value>::type* = nullptr){
+cleaner make_cleaner(cleaner next,T&& obj,typename std::enable_if<!std::is_trivially_destructible<T>::value &&
+                                            !std::is_function<typename std::remove_reference<T>::type>::value &&
+                                            !has_function_operator<typename std::remove_reference<T>::type>::value>::type* = nullptr){
     return make_object_cleaner(std::move(next),std::move(obj));
 }
+template<typename T>
+cleaner make_cleaner(T&& obj,typename std::enable_if<!std::is_trivially_destructible<typename std::remove_reference<T>::type>::value &&
+                     !std::is_function<typename std::remove_reference<T>::type>::value &&
+                     !has_function_operator<typename std::remove_reference<T>::type>::value>::type* = nullptr){
+    return make_object_cleaner(std::move(obj));
+}
+
 #endif // XHBLIB_CLEANER_H_
---
+++ cleaner_test.cpp
@@ -0,0 +1,105 @@
+#ifdef XHBLIB_CLEANER_UNIT_TEST
+
+#include "cleaner.h"
+
+
+#include <cassert>
+#include <cstdlib>
+#include <type_traits>
+using namespace std;
+
+void* test_normal_function_global = malloc(10);
+void test_normal_function(){
+    free(test_normal_function_global);
+}
+void test_simple_cleaner(){
+    {
+        void* p = malloc(10);
+        auto c = make_cleaner(p);
+    }
+    assert(assert_ss.str() == "f");
+    assert_ss.str("");
+    
+    {
+        char* p = static_cast<char*>(malloc(10));
+        auto c = make_cleaner(p);
+    }
+    assert(assert_ss.str() == "f");
+    assert_ss.str("");
+    
+    {
+        void* p = malloc(10);
+        auto c= make_cleaner([p]()mutable{free(p);});
+    }
+    assert(assert_ss.str() == "l");
+    assert_ss.str("");
+    
+    {
+        struct test_functor{
+            void* ptr = nullptr;
+            test_functor(void* p):ptr(p){}
+            void operator()(){
+                free(ptr);
+            }
+        };
+        
+        void* p = malloc(10);
+        
+        auto c = make_cleaner(test_functor(p));
+    }
+    assert(assert_ss.str() == "l");
+    assert_ss.str("");
+    
+    {
+        auto c = make_cleaner(test_normal_function);
+    }
+    assert(assert_ss.str() == "l");
+    assert_ss.str("");
+    
+    {
+        struct test_object{
+            ~test_object(){}
+        };
+        test_object obj;
+        auto c = make_cleaner(obj);
+    }
+    assert(assert_ss.str() == "o");
+    assert_ss.str("");
+
+}
+void test_next_cleaner(){
+    {
+        void* p = malloc(10);
+        auto f = make_cleaner(p);
+        
+        char* pc = static_cast<char*>(malloc(100));
+        auto lf = make_cleaner(move(f),pc);
+        
+        struct test_object{
+            ~test_object(){}
+        };
+        
+        test_object obj;
+        
+        auto olf = make_cleaner(move(lf), move(obj));
+        
+        auto po = new test_object();
+        auto lolf = make_cleaner(move(olf), [po]()mutable{delete po;});
+        
+        auto llolf = make_cleaner(move(lolf), [](){cout<<"do nothing"<<endl;});
+        
+        
+    }
+    assert(assert_ss.str() == "llolf");
+    assert_ss.str("");
+}
+
+
+void test_construct(){
+    
+}
+int main(){
+    test_simple_cleaner();
+    test_next_cleaner();
+}
+#endif // XHBLIB_CLEANER_UNIT_TEST
