31b8d0f507f4ceeef6308d1de1fcd431514b4b5e
downmooner <11she_232@163.com>
2017-04-19 21:32:27 +0800

    add log tool

--- src/log.cpp
+++ src/log.cpp
@@ -1,9 +1,15 @@
 #include "log.h"
-
-#include <string>
+#include <utility>
+#include <sstream>
+#include <chrono>
+#include <ctime> // localtime
+#include <cstdio> // sprintf
 
 namespace xhb{
     const char* log_level_name[] = {"error","warn","info","debug","trace"};
+    const char* level_to_name(log_level lev){
+        return log_level_name[static_cast<int>(lev)];
+    }
 }
 
 using namespace xhb;
@@ -27,3 +33,90 @@ std::istream& operator>>(std::istream& in,log_level& lev){
     in.setstate(std::ios::failbit);
     return in;
 }
+
+namespace xhb{
+
+std::atomic<int> logger::_idx = {0};
+
+logger::logger(){
+    _name = "default" + std::to_string(_idx++.load(std::memory_order_relaxed));
+    log_registry::instance().register(this);  
+}
+
+logger::logger(const string& name):_name(name){
+    log_registry::instance().register(this);
+}
+
+logger::~logger(){
+    log_registry::instance().unregister(this);
+}
+
+void xhb_printf(std::ostringstream& out,const char* fmt){
+    out<<fmt;
+}
+
+template<typename T,typename... E>
+void xhb_printf(std::ostringstream& out,const char* fmt, T val,E else_args){
+    for(; *fmt != '\0'; ++fmt){
+        if(*fmt == '%'){
+            if(*(fmt + 1) == '%')
+                ++fmt;
+            else{
+                out << value;
+                xhb_printf(out,fmt + 1,else_args...);
+                return;
+            }
+        }
+        out << *fmt;
+    }
+}
+template<typename...A>
+void logger::log(log_level lev,const char* fmt,A... args){
+    if(is_logable(lev) == false) 
+        return;
+    bool is_std_enable = _stdout.load(std::memory_order_relaxed);
+    if(is_std_enable == false)
+        return;
+    std::ostringstream out;
+    out   <<level_to_name << " ";
+
+    auto now = std::chrono::system_clock::now();
+    auto ms = std::chrono::duration_cast<std::chrono::millisecond>(now.time_since_epoch()).count() % 1000;
+    auto tt = std::chrono::system_clock::to_time_t(now);
+    auto* tm = std::localtime(&tt);
+    char tp[128];
+    std::snprintf(tp,sizeof(tp),"%04d-%02d-%02d %02d:%02d:%02d,%03d",
+        tm->tm_year + 1900,tm->tm_mon + 1, tm->tm_mday,
+        tm->tm_hour,tm->tm_min,tm->tm_sec,
+        ms);
+    out << tp << " " << std::this_thread::get_id() << ": ";
+   
+    xhb_printf(out,fmt,args...)
+    
+    out << "\n";
+    std::cout << out.str();
+}
+
+// log_registry
+log_registry& log_registry::instance(){
+    static log_registry ins;
+    return ins;
+}
+
+void log_registry::register(logger* log){
+     std::lock_guard<std::mutex> lock(_mutex);
+     _loggers[log->name()] = log;
+}
+
+void log_registry::unregister(logger* log){
+    std::lock_guard<std::mutex> lock(_mutex);
+    _loggers.erase(l.name());
+}
+
+void log_registry::move(logger* to,logger* from){
+    std::lock_guard<std::mutex> lock(_mutex);
+
+}
+
+
+}// xhb namespace
--- src/log.h
+++ src/log.h
@@ -2,6 +2,11 @@
 #define XHBLIB_LOG_H_
 
 #include <iostream>
+#include <string>
+#include <mutex>
+#include <atomic>
+#include <unordered_map>
+#include <vector>
 
 namespace xhb{
 
@@ -15,6 +20,57 @@ enum class log_level{
 } // xhb namespace
 
 std::ostream& operator<<(std::ostream& out,xhb::log_level lev);
-std::istream& operator>>(std::istream& in,xhb::log_level lev);
+std::istream& operator>>(std::istream& in,xhb::log_level& lev);
 
+namespace xhb{
+
+class log_registry;
+
+class logger final{
+    std::string _name;
+    std::atomic<log_level> _level = {log_level::info};
+    static std::atomic<bool> _stdout;
+    static std::atomic<int> _idx;
+public:
+    logger();
+    logger(const string& name);
+    ~logger();
+
+    inline string name() const {return _name;}
+    inline log_level level() const { return _level.load(std::memory_order_relaxed);}
+    inline void set_level(log_level lev) {_level.store(lev,std::memory_order_relaxed);}
+    inline bool is_logable(log_level lev) const {
+        return lev <= _level.load(std::memory_order_relaxed);
+    }
+
+    inline static void set_stdout_enable(bool b){
+        _stdout.store(b,std::memory_order_relaxed);
+    }
+
+    template<typename... A>
+    void log(log_level lev,const char* fmt, A&&... args);
+};
+
+class log_registry final{
+    mutable std::mutex _mutex;
+    std::unordered_map<std::string,logger*> _loggers;
+private:
+    log_registry(){};
+public:
+    static log_registry& instance();
+
+    void register(logger* log);
+    void unregister(logger* log);
+
+    void set_levels(log_level lev);
+    
+    log_level get_level(const std::string& name)const;
+    void set_level(const std::string& name,log_level lev);
+
+    std::vector<std::string> get_names()const;
+
+};
+
+
+} // xhb namespace
 #endif //XHBLIB_LOG_H_
