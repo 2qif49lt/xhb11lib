48ceb83fe18dcc121cf17ea69f093caf9ba8ec95
downmooner <11she_232@163.com>
2017-05-08 20:36:23 +0800

    修正在windows x86环境下使用x64函数

--- src/utility/bitopt.h
+++ src/utility/bitopt.h
@@ -9,7 +9,7 @@
 	assert(xhb::ctz((unsigned short)12) == 2);
 	assert(xhb::clz((unsigned short)12) == 12);
 	assert(xhb::ctz((unsigned long)12) == 2);
-	assert(xhb::clz((unsigned long)12) == 60);
+	assert(xhb::clz((unsigned long)12) == 60); // 在window系统long始终是32位，这里应该是28
 	assert(xhb::ctz((unsigned long long)12) == 2);
 	assert(xhb::clz((unsigned long long)12) == 60);
 */
@@ -37,7 +37,7 @@ inline unsigned int clz(unsigned int val) {
 
     #ifdef _MSC_VER
     unsigned long leading_zeros = 0;
-    return _BitScanReverse(&leading_zero, val) ? (unsigned int)(bits -1 - leading_zeros) : bits;
+    return _BitScanReverse(&leading_zeros, val) ? (unsigned int)(bits -1 - leading_zeros) : bits;
     #else
     return val ? (unsigned int)__builtin_clz(val) : bits;
     #endif
@@ -48,7 +48,7 @@ inline unsigned int clz(unsigned short val) {
 
     #ifdef _MSC_VER
     unsigned long leading_zeros = 0;
-    return _BitScanReverse(&leading_zero, val) ? (unsigned int)(bits -1 - leading_zeros) : bits;
+    return _BitScanReverse(&leading_zeros, val) ? (unsigned int)(bits -1 - leading_zeros) : bits;
     #else
     return val ? (unsigned int)__builtin_clz((unsigned int)val) - cast_zeros : bits;
     #endif
@@ -58,7 +58,7 @@ inline unsigned int clz(unsigned long val) {
 
     #ifdef _MSC_VER
     unsigned long leading_zeros = 0;
-    return _BitScanReverse(&leading_zero, val) ? (unsigned int)(bits -1 - leading_zeros) : bits;
+    return _BitScanReverse(&leading_zeros, val) ? (unsigned int)(bits -1 - leading_zeros) : bits;
     #else
     return val ? (unsigned int)__builtin_clzl(val) : bits;
     #endif
@@ -67,8 +67,14 @@ inline unsigned int clz(unsigned long long val) {
     constexpr unsigned int bits =  std::numeric_limits<unsigned long long>::digits;
 
     #ifdef _MSC_VER
+    #ifdef _WIN64
     unsigned long leading_zeros = 0;
-    return _BitScanReverse64(&leading_zero, val) ? (unsigned int)(bits -1 - leading_zeros) : bits;
+    return _BitScanReverse64(&leading_zeros, val) ? (unsigned int)(bits -1 - leading_zeros) : bits;
+    #else
+    unsigned int rh = (unsigned int)((val << 32) >> 32);
+    unsigned int lh = (unsigned int)(val >> 32);
+    return lh ? clz(lh) : clz(rh) + 32;
+    #endif
     #else
     return val ? (unsigned int)__builtin_clzll(val) : bits;
     #endif
@@ -110,8 +116,14 @@ inline unsigned int ctz(unsigned long long val) {
     constexpr unsigned int bits =  std::numeric_limits<unsigned int>::digits;
 
     #ifdef _MSC_VER
+    #ifdef _WIN64
     unsigned long trailing_zeros = 0;
     return _BitScanForward64(&trailing_zeros, val) ? (unsigned int)(trailing_zeros) : bits;
+    #else
+    unsigned int rh = (unsigned int)((val << 32) >> 32);
+    unsigned int lh = (unsigned int)(val >> 32);
+    return rh ? ctz(rh) : ctz(ls) + 32;
+    #endif
     #else
     return val ? (unsigned int)__builtin_ctzll(val) : bits;
     #endif
