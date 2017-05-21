8b8c77e570019c76799da39102789effd569f7bc
downmooner <11she_232@163.com>
2017-05-21 17:29:20 +0800

    添加nullopt 引用

--- src/utility/optional.h
+++ src/utility/optional.h
@@ -4,9 +4,11 @@
 #if (defined __cplusplus) && (__cplusplus >= 201700L)
 #include <optional>
 using std::optional;
+using std::nullopt;
 #else
 #include <experimental/optional>
 using std::experimental::optional;
+using std::experimental::nullopt;
 #endif
 
 #endif // XHBLIB_UTILITY_OPTIONAL_H_
