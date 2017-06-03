3abbd6091696b6293c72d4517678bfe7ec09dba3
downmooner <11she_232@163.com>
2017-06-03 18:06:27 +0800

    增加对指针类型值进行对齐的特化

--- src/utility/align.h
+++ src/utility/align.h
@@ -14,10 +14,25 @@ inline constexpr T align_up(T v) {
     return (v + A - 1) & (~(A - 1));
 }
 
+// 指针类型
+template <size_t A, typename T>
+inline constexpr
+T* align_up(T* v) {
+    static_assert(sizeof(T) == 1, "align byte pointers only");
+    return reinterpret_cast<T*>(align_up<A>(reinterpret_cast<uintptr_t>(v)));
+}
+
 template<size_t A, typename T>
 inline T constexpr align_down(T v) {
 	static_assert((A & (A - 1)) == 0, "parameter must be  a power of 2.");
     return v & (~(A - 1));
+}
+
+template <size_t A, typename T>
+inline constexpr
+T* align_down(T* v) {
+    static_assert(sizeof(T) == 1, "align byte pointers only");
+    return reinterpret_cast<T*>(align_down<A>(reinterpret_cast<uintptr_t>(v)));
 }
 
 // 通用
@@ -25,12 +40,25 @@ template<typename T>
 T align_up(T num, size_t mod) {
 	return mod * ((num + mod - 1) / mod);
 }
+
+template <typename T>
+inline constexpr
+T* align_up(T* v, size_t mod) {
+    static_assert(sizeof(T) == 1, "align byte pointers only");
+    return reinterpret_cast<T*>(align_up(reinterpret_cast<uintptr_t>(v), mod));
+}
+
 template<typename T>
 T align_down(T num, size_t mod) {
 	return mod * (num / mod);
 }
 
-
+template <typename T>
+inline constexpr
+T* align_down(T* v, size_t mod) {
+    static_assert(sizeof(T) == 1, "align byte pointers only");
+    return reinterpret_cast<T*>(align_down(reinterpret_cast<uintptr_t>(v), mod));
+}
 } // xhb namespace
 
 
