39a1449f8cfaf845d55cb07bef803155d88904c3
downmooner <11she_232@163.com>
2017-06-05 16:50:35 +0800

    增加返回unique_ptr的工具

--- src/utility/memory_align.h
+++ src/utility/memory_align.h
@@ -9,4 +9,34 @@ extern int posix_memalign(void **ptr, size_t align, size_t size);
 #include <stdlib.h> //  posix_memalign
 #endif
 
+#include <cassert>
+#include <memory>
+#include <new> // bad_alloc
+#include <stdexcept>
+namespace xhb {
+
+
+struct free_deleter {
+    void operator()(void* p) { ::free(p); }
+};
+
+template <typename CharType>
+inline
+std::unique_ptr<CharType[], free_deleter> allocate_aligned_buffer(size_t size, size_t align) {
+    static_assert(sizeof(CharType) == 1, "must allocate byte type");
+    void* ret;
+    auto r = posix_memalign(&ret, align, size);
+    if (r == ENOMEM) {
+        throw std::bad_alloc();
+    } else if (r == EINVAL) {
+        throw std::runtime_error(sprint("Invalid alignment of %d; allocating %d bytes", align, size));
+    } else {
+        assert(r == 0);
+        return std::unique_ptr<CharType[], free_deleter>(reinterpret_cast<CharType *>(ret));
+    }
+}
+
+
+} // xhb namespace
+
 #endif // XHBLIB_UTILITY_MEMORY_ALIGN_H_
