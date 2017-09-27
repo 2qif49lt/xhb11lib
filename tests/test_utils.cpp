c10f25f8e3bf5be05bcab9700f3629f784fc55b9
downmooner <11she_232@163.com>
2017-09-27 15:51:16 +0800

    修改增加输出测试项目耗时

--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <chrono>
+namespace stdcho = std::chrono;
 
 #include "test_utils.h"
 
@@ -20,8 +22,15 @@ test_base::test_base() {
 
 int main (int argc, char** argv) {
     std::for_each(tests.begin(), tests.end(), [argc, argv](xhb::test_base* test) { 
+       
+        auto tbeg = stdcho::high_resolution_clock::now();
         int ret = test->run(argc, argv);
-        std::cout << std::left << std::setw(5) << ret << test->get_name() << ":" << test->get_test_file() << std::endl;
+        auto tend = stdcho::high_resolution_clock::now();
+
+        std::cout << std::left << std::setw(5) << ret << test->get_name() 
+            << ":" << test->get_test_file() 
+            << " cost(us): " << stdcho::duration_cast<stdcho::microseconds>(tend - tbeg).count()
+            << std::endl;
         assert(ret == 0);
         });
 }
