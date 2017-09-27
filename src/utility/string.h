d6b6b3ad42c23e6f6bc7eeb6753ff1118680e0cf
downmooner <11she_232@163.com>
2017-09-27 14:47:06 +0800

    添加string工具

--- src/utility/string.h
+++ src/utility/string.h
@@ -7,24 +7,24 @@
 #include <iterator>
 
 namespace xhb {
-    template <typename It>
-    void split(const std::string& s, char delim, It inserter, bool empty) {
-        std::stringstream ss(s);
-        std::string block;
-        
-        while (std::getline(ss, block, delim)) {
-            if (empty == false && block == "") {
-                continue;
-            }
-            *(inserter++) = block;
+template <typename It>
+void split(const std::string& s, char delim, It inserter, bool empty) {
+    std::stringstream ss(s);
+    std::string block;
+    
+    while (std::getline(ss, block, delim)) {
+        if (empty == false && block == "") {
+            continue;
         }
+        *(inserter++) = block;
+    }
 
-    }
-    inline std::vector<std::string> split(const std::string& s, char delim, bool empty = false) {
-        std::vector<std::string> vec;
-        split(s, delim, std::back_inserter(vec), empty);
-        return vec;
-    }
+}
+inline std::vector<std::string> split(const std::string& s, char delim, bool empty = false) {
+    std::vector<std::string> vec;
+    split(s, delim, std::back_inserter(vec), empty);
+    return vec;
+}
 
 } // xhb namespace
 
